9a0f9a216d957fa9ca8685e8e50f14ab417e389b
SaiAshish9 <saiashish3760@gmail.com>
2020-07-09 22:23:26 +0530

    fixed bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ const styles = (theme) => ({
 });
 
 function Header(props) {
-  const { classes, onDrawerToggle } = props;
+  // const { classes, onDrawerToggle } = props;
 
   return (
     <React.Fragment>
